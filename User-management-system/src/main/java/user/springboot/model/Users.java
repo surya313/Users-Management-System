4d93ef61a4784eb044a834fea873651b83aa64fe
surya313; <r.suryaprakash31@gmail.com>
2022-04-21 15:58:02 +0100

    generationType

--- User-management-system/src/main/java/user/springboot/model/Users.java
+++ User-management-system/src/main/java/user/springboot/model/Users.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotBlank;
 public class Users {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	private String enteredTime;
 	private String lastUpdated;
